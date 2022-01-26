cfefdaa610503b24ace6719fda5d2ef0ac753db0
Jose Antonio Pereiro Morejon <44244693+josePereiro@users.noreply.github.com>
2022-01-26 15:22:32 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,23 @@
+# Install
+let
+    dir = pwd()
+    @info("Testing", dir)
+    
+    cmdstr = """\
+    git clone --depth 1 --branch main --single-branch \
+    https://github.com/josePereiro/Chemostat_EColi.jl Chemostat_EColi && \
+    cd Chemostat_EColi && \
+    julia --startup-file=no scripts/0.1_install.jl\
+    """
+    
+    run(Cmd(`bash -c $(cmdstr)`); wait = true)
+end
+
+# Import
 using Chemostat_EColi
 using Test
 
+# Tests
 @testset "Chemostat_EColi.jl" begin
-    # Write your tests here.
+    @test true
 end
